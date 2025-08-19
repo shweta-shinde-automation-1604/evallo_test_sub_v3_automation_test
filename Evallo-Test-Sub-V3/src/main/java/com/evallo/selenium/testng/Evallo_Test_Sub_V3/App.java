df113ee2dde9f8168fe012b64a41774d48e17d2a
shweta-shinde-automation-1604 <shweta@evallo.ai>
2025-08-19 15:14:10 +0530

    First Commit

---
+++ Evallo-Test-Sub-V3/src/main/java/com/evallo/selenium/testng/Evallo_Test_Sub_V3/App.java
@@ -0,0 +1,10 @@
+package com.evallo.selenium.testng.Evallo_Test_Sub_V3;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
